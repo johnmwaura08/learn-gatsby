2dca0a0afc0231e985355e426dc6f95b27dcdae9
John Mwaura <johnmwaura08@gmail.com>
2020-11-27 08:49:47 -0600

    use styled components with gastby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { ExampleButton } from "../components/button"
 import Layout from "../components/layout"
 
 export default function Home() {
   return (
     <Layout>
       <h1>Hello from gatsby</h1>
+      <ExampleButton>click me</ExampleButton>
     </Layout>
   )
 }
