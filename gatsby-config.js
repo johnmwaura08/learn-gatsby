c8b99510dda27283dcbb7db4a0bb2b19602ce8dd
John Mwaura <johnmwaura08@gmail.com>
2020-11-27 14:50:25 -0600

    setting up graphql plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@
 
 module.exports = {
   /* Your site config here */
+  //always make sure the properties you are adding match whatever you have in the list
   plugins: [`gatsby-plugin-styled-components`],
+  siteMetadata: {
+    title: 'Gatsby Tutorial',
+    description: 'random description',
+    author : '@kichwakubwa',
+    data: ['item1', 'item 400'],
+    person: {name : 'jonte', age : '45'}
+  }
 }
